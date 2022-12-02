0ca10ba599616c70a8c3db840916c4e419247116
Joseph Kakai <josephngalu96gmail.com>
2022-12-02 12:28:34 +0300

    Added a repeter module

---
+++ repeeter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Defines a "repeat" function that takes 2 arguments.
+def repeat(s, exclaim):
+    """
+    Returns the string 's' repeated 3 times.
+    If exclaim is true, add exclamation marks.
+    """
+
+    result = s + s + s # can also use "s * 3" which is faster (Why?)
+    if exclaim:
+        result = result + '!!!'
+    return result
